18878b1c2a833a90c27c315424a20541abb7557c
jamangstangs <jamangsong@gmail.com>
2022-05-06 00:43:23 +0900

    ItemRepository 구현

--- src/main/java/com/nanum/nanumserver/Item/ItemRepository.java
+++ src/main/java/com/nanum/nanumserver/Item/ItemRepository.java
@@ -1,9 +1,12 @@
 package com.nanum.nanumserver.Item;
 
+import com.nanum.nanumserver.Item.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ItemRepository extends JpaRepository<Item,Long> {
-
+    Optional<Item> findByItemId(Long itemId);
 }
