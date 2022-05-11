7d3fe4f73b199fed6d526c3b5b1d93be36695740
jamangstangs <jamangsong@gmail.com>
2022-05-11 23:26:46 +0900

    User 클래스 구현

--- src/main/java/com/nanum/nanumserver/user/domain/User.java
+++ src/main/java/com/nanum/nanumserver/user/domain/User.java
@@ -2,10 +2,32 @@ package com.nanum.nanumserver.user.domain;
 
 import com.nanum.nanumserver.common.persistence.BaseEntity;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 
 @Entity
 public class User extends BaseEntity {
+    @Column(unique = true)
+    private String username;
+    private String password;
 
+    protected User() {
+    }
 
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
