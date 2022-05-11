e114d896f08a48af3b77cd5900756d5841f0f917
jamangstangs <jamangsong@gmail.com>
2022-05-11 23:30:12 +0900

    SimpleUser 클래스 구현

--- src/main/java/com/nanum/nanumserver/user/domain/SimpleUser.java
+++ src/main/java/com/nanum/nanumserver/user/domain/SimpleUser.java
@@ -6,6 +6,11 @@ public class SimpleUser implements Serializable {
     private final Long id;
 
     public SimpleUser(User user) {
-
+        this.id = user.getId();
     }
+
+    public Long getId() {
+        return id;
+    }
+
 }
