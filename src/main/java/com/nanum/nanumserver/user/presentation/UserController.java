772165151f42e660ea9707ce566a8aa517f28704
jamangstangs <jamangsong@gmail.com>
2022-05-12 01:21:49 +0900

    UserController - login - logout - register

--- src/main/java/com/nanum/nanumserver/user/presentation/UserController.java
+++ src/main/java/com/nanum/nanumserver/user/presentation/UserController.java
@@ -2,7 +2,13 @@ package com.nanum.nanumserver.user.presentation;
 
 import com.nanum.nanumserver.user.application.LoginService;
 import com.nanum.nanumserver.user.application.UserService;
+import com.nanum.nanumserver.user.dto.request.SignInRequest;
+import com.nanum.nanumserver.user.dto.request.SignUpRequest;
 import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,4 +19,29 @@ public class UserController {
 
     private final UserService userService;
     private final LoginService loginService;
+
+    @PostMapping("/users")
+    public ResponseEntity<Void> register(@Validated @RequestBody SignUpRequest signUpRequest) {
+        userService.signUP(signUpRequest);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<Void> login(@Validated @RequestBody SignInRequest request) {
+        loginService.login(request);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PostMapping("/logout")
+    public ResponseEntity<Void> logout() {
+        loginService.logout();
+        return ResponseEntity.noContent().build();
+    }
+
+    // 유저 검색
+    // 내 계정 검색
+
+
+
+
 }
